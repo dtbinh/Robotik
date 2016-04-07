a9f2dff5a080966f93962d3ee13a846b2a5678a7
Stefan <xoomer@web.de>
2016-04-07 22:59:18 +0200

    Toad prototype, House working

--- ue1/a6/House.java
+++ ue1/a6/House.java
@@ -1,21 +1,28 @@
+import lejos.nxt.Motor;
+import lejos.robotics.navigation.DifferentialPilot;
+import lejos.nxt.Sound;
+
 public class House {
     public static void main(String args[]) {
-        RoboUtil.driveForCentimeters(50);
-        RoboUtil.rotateForDegrees(135);
-        RoboUtil.driveForCentimeters((int) (50 * Math.sqrt(2)));
-        RoboUtil.rotateForDegrees(-135);
-        RoboUtil.driveForCentimeters(50);
-        RoboUtil.rotateForDegrees(-45);
-        RoboUtil.driveForCentimeters((int) (50 / Math.sqrt(2)));
-        RoboUtil.rotateForDegrees(-90);
-        RoboUtil.driveForCentimeters((int) (50 / Math.sqrt(2)));
-        RoboUtil.rotateForDegrees(-135);
-        RoboUtil.driveForCentimeters(50);
-        RoboUtil.rotateForDegrees(135);
-        RoboUtil.driveForCentimeters((int) (50 * Math.sqrt(2)));
-        RoboUtil.rotateForDegrees(-135);
-        RoboUtil.driveForCentimeters(50);
-        RoboUtil.rotateForDegrees(180);
-        RoboUtil.driveForCentimeters(50);
+        DifferentialPilot pilot = new DifferentialPilot(DifferentialPilot.WHEEL_SIZE_NXT1, 11.3, Motor.C, Motor.A);
+        System.out.println(DifferentialPilot.WHEEL_SIZE_NXT2);
+        pilot.travel(50);                   // Move forward
+        pilot.rotate(-135);                 // Turn to diagonal
+        pilot.travel(50 * Math.sqrt(2));    // Drive diagonal
+        pilot.rotate(135);                  // Turn to side
+        pilot.travel(50);                   // Drive side
+        pilot.rotate(45);                   // Turn to roof
+        pilot.travel(50 / Math.sqrt(2));    // Drive roof
+        pilot.rotate(90);                   // Turn at roof
+        pilot.travel(50 / Math.sqrt(2));    // Drive roof
+        pilot.rotate(135);                  // Turn to side
+        pilot.travel(50);                   // Drive side
+        pilot.rotate(-135);                 // Turn to diagonal
+        pilot.travel(50 * Math.sqrt(2));    // Drive diagonal
+        pilot.rotate(135);                  // Turn to side
+        pilot.travel(50);                   // Drive side
+        pilot.rotate(180);                  // Turn around
+        pilot.travel(50);                   // Drive side
+        Sound.beep();                       // Beep
     }
 }
--- ue2/a2/Toad.java
+++ ue2/a2/Toad.java
@@ -2,29 +2,82 @@ import lejos.nxt.SensorPort;
 import lejos.nxt.TouchSensor;
 import lejos.nxt.Button;
 import lejos.nxt.Motor;
+import lejos.robotics.navigation.DifferentialPilot;
 
 public class Toad {
     public static void main(String[] args) {
+        byte wallRight = 0; // defines, if the found wall is to the left or the right of the NXT
+        byte mode = 0; // mode = 0: find wall; mode = 1: follow wall; mode = 2: check for corner;
+        byte stepDegrees = 5;
+        byte cmPerSecond = 10;
+        byte checkDistance = 20;
+        byte leftPressed = 0;
+
         TouchSensor leftSensor = new TouchSensor(SensorPort.S4);
         TouchSensor rightSensor = new TouchSensor(SensorPort.S1);
-        Motor.A.setSpeed(360);
-        Motor.C.setSpeed(360);
+        DifferentialPilot pilot = new DifferentialPilot(DifferentialPilot.WHEEL_SIZE_NXT1, 11.3, Motor.C, Motor.A);
+
+        pilot.setTravelSpeed(cmPerSecond);
+
         while (!Button.ESCAPE.isDown()) {
-            if (!Motor.A.isMoving() || !Motor.C.isMoving()) {
-                Motor.A.forward();
-                Motor.C.forward();
+            if (!pilot.isMoving()) {
+                pilot.forward();
             }
-            if (leftSensor.isPressed()) {
-                Motor.A.stop();
-                Motor.C.stop();
-                RoboUtil.driveForCentimeters(-4);
-                RoboUtil.rotateForDegrees(5);
+
+            if (leftSensor.isPressed() || rightSensor.isPressed()) {
+                leftPressed = leftSensor.isPressed() ? (byte) 1 : (byte) -1;
+                if (mode == 0) {
+                    wallRight = (byte) -leftPressed;
+                    mode = 1;
+                }
+
+                if (mode == 1) {
+                    pilot.stop();
+                    if ((wallRight == 1 && leftPressed == 1) || (wallRight == -1 && leftPressed == -1)) {
+                        pilot.travel(-10);
+                        //pilot.arc(wallRight * 8, -wallRight * 85);
+                        if (wallRight == 1) {
+                            pilot.arc(8, 85);
+                        }
+                        else if (wallRight == -1) {
+                            pilot.arc(-8, -85);
+                        }
+                    }
+                    else {
+                        pilot.travel(-4);
+                        pilot.rotate(wallRight * stepDegrees);
+                    }
+                }
+
+                else if (mode == 2) {
+                    pilot.stop();
+                    pilot.travel(-10);
+                    //pilot.arc(wallRight * 8, -wallRight * 85);
+                    if (wallRight == 1) {
+                        pilot.arc(8, 85);
+                    }
+                    else if (wallRight == -1)  {
+                        pilot.arc(-8, -85);
+                    }
+                    mode = 1;
+                }
             }
-            if (rightSensor.isPressed()) {
-                Motor.A.stop();
-                Motor.C.stop();
-                RoboUtil.driveForCentimeters(-4);
-                RoboUtil.rotateForDegrees(-5);
+
+            if (mode == 2 && pilot.isMoving() && pilot.getMovementIncrement() > 0.8 * checkDistance) {
+                mode = 1;
+            }
+
+            if (mode == 1 && pilot.isMoving() && pilot.getMovementIncrement() > checkDistance) {
+                pilot.stop();
+                //pilot.arc(wallRight * 8, -wallRight * 85);
+                if (wallRight == 1) {
+                    pilot.arc(8, 85);
+                }
+                else if (wallRight == -1) {
+                    pilot.arc(-8, -85);
+                }
+                pilot.rotate(180);
+                mode = 2;
             }
         }
     }
